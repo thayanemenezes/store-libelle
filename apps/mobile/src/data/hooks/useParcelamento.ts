518b6b5e145a16d09f4e383e11613c87690c897f
Thayane Menezes <thayane.nascimento@solution4fleet.com.br>
2024-10-15 22:52:37 -0300

    feat: remove mobile

--- apps/mobile/src/data/hooks/useParcelamento.ts
+++
@@ -1,5 +0,0 @@
-import { CalcularParcelamento } from '@gstore/core'
-
-export default function useParcelamento(valor: number, quantidade: number = 12) {
-    return new CalcularParcelamento().executar(valor, quantidade)
-}
